d116dfec605a61bdae16f32e1d537ddde57860ac
RemRem <remi.zapp.rz@gmail.com>
2025-01-26 21:14:06 +0100

    Initial commit

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  template: ` <p>button works!</p> `,
+  styles: ``,
+  standalone: true,
+})
+export class ButtonComponent {}
